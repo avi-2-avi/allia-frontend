a37a3c4cbac1e9b66cc1f80ccbae9a22adb961c1
alanegd <alangalavisd@gmail.com>
2024-12-07 12:03:20 -0500

    feat: Add toastify

--- src/app/components/form/ChatPrompt.tsx
+++ src/app/components/form/ChatPrompt.tsx
@@ -1,49 +1,64 @@
 "use client";
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent } from "react";
 import CircleButton from "@/app/components/ui/CircleButton";
 import Image from "next/image";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ChatPrompt: React.FC = () => {
-    const [prompt, setPrompt] = useState<string>(''); // State for input value
+  const [prompt, setPrompt] = useState<string>("");
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (prompt.trim() === '') {
-            alert('Please enter a prompt before submitting!');
-            return;
-        }
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (prompt.trim() === "") {
+      toast.warning("Please enter a prompt before submitting!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
+    }
 
-        console.log('Do something:', prompt);
-        setPrompt('');
-    };
+    console.log("Do something:", prompt);
+    setPrompt("");
+  };
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setPrompt(event.target.value);
-    };
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPrompt(event.target.value);
+  };
 
-    return (
-        <form
-            className="flex space-x-2.5 w-full"
-            onSubmit={handleSubmit} // Attach the onSubmit handler
-        >
-            <Image src="/icons/Plus.svg" alt="Plus icon" width={20} height={20} />
-            <input
-                className="flex-grow bg-secondary py-2 px-6 border border-input-border rounded-full"
-                type="text"
-                id="prompt"
-                name="prompt"
-                placeholder="Enter your prompt"
-                value={prompt}
-                onChange={handleChange}
+  return (
+    <div>
+      <ToastContainer />
+      <form className="flex space-x-2.5 w-full" onSubmit={handleSubmit}>
+        <Image src="/icons/Plus.svg" alt="Plus icon" width={20} height={20} />
+        <input
+          className="flex-grow bg-secondary py-2 px-6 border border-input-border rounded-full"
+          type="text"
+          id="prompt"
+          name="prompt"
+          placeholder="Enter your prompt"
+          value={prompt}
+          onChange={handleChange}
+        />
+        <CircleButton
+          type="submit"
+          image={
+            <Image
+              src="/icons/Send.svg"
+              width={20}
+              height={20}
+              alt="Send icon"
             />
-            <CircleButton
-                type="submit"
-                image={
-                    <Image src="/icons/Send.svg" width={20} height={20} alt="Send icon" />
-                }
-            />
-        </form>
-    );
+          }
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ChatPrompt;
