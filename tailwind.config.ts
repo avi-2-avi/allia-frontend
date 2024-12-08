f0ad2d5652680cf8d821b07f10fed9bd2ff3ee7d
Cristina <avicon95@protonmail.com>
2024-12-07 20:35:39 -0500

    chore: colors update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,37 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      transparent: "transparent",
-      background: "var(--background)",
-      foreground: {
-       DEFAULT:   "var(--foreground)",
-        muted: "var(--foreground-muted)",
-      },
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        accent: "var(--accent)",
-        warning: {
-          DEFAULT: "var(--warning)",
-          muted: "var(--warning-muted)",
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: "transparent",
+            background: "var(--background)",
+            foreground: {
+                DEFAULT: "var(--foreground)",
+                muted: "var(--foreground-muted)",
+                secondary: "var(--foreground-secondary)",
+            },
+            white: "var(--white)",
+            black: "var(--black)",
+            primary: "var(--primary)",
+            secondary: "var(--secondary)",
+            tertiary: "var(--tertiary)",
+            accent: "var(--accent)",
+            warning: {
+                DEFAULT: "var(--warning)",
+                muted: "var(--warning-muted)",
+            },
+            danger: {
+                DEFAULT: "var(--danger)",
+                muted: "var(--danger-muted)",
+            },
+            "input-border": "var(--input-border)"
         },
-        danger: {
-          DEFAULT: "var(--danger)",
-          muted: "var(--danger-muted)",
-        },
-      "input-border": "var(--input-border)"
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
