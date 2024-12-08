e15d3651f064ad0379e37aca3de8c8a31701733e
Cristina <avicon95@protonmail.com>
2024-12-07 22:07:30 -0500

    feat: feature section

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ButtonStyles = {
   PRIMARY:
-    "bg-primary border-2 hover:bg-transparent hover:text-primary border border-primary",
+      "bg-primary border-2 hover:bg-transparent hover:text-primary border border-primary",
   SECONDARY:
-    "bg-transparent border-2 border-foreground-muted hover:bg-primary hover:text-foreground hover:border-primary",
-    TERTIARY:
-    "bg-accent text-background border-2 hover:bg-transparent hover:text-accent border-accent",
+      "bg-transparent border-2 border-foreground-muted hover:bg-primary hover:text-foreground hover:border-primary",
+  TERTIARY:
+      "bg-accent text-background border-2 hover:bg-transparent hover:text-accent border-accent",
 };
 
 interface ButtonProps {
@@ -15,25 +15,28 @@ interface ButtonProps {
   onClick?: () => void;
   disabled?: boolean;
   className?: string;
+  type?: "button" | "submit" | "reset";
 }
 
 const Button: React.FC<ButtonProps> = ({
-  text,
-  style,
-  onClick,
-  disabled,
-  className,
-}) => {
+                                         text,
+                                         style,
+                                         onClick,
+                                         disabled,
+                                         className,
+                                         type = "button",
+                                       }) => {
   return (
-    <button
-      className={`rounded-xl transition duration-300 p-2.5 ${
-        ButtonStyles[style]
-      } ${className}  ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {text}
-    </button>
+      <button
+          className={`rounded-xl transition duration-300 p-2.5 ${
+              ButtonStyles[style]
+          } ${className}  ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
+          onClick={onClick}
+          disabled={disabled}
+          type={type}
+      >
+        {text}
+      </button>
   );
 };
 
