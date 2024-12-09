614fcf450dad357df83e244eabf8717d8a0e4d66
alanegd <alangalavisd@gmail.com>
2024-12-08 20:04:52 -0500

    feat: Add chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,31 +1,55 @@
-// chat/page.tsx
-import Link from 'next/link';
+"use client";
+import React, { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import Sidebar from "@/app/components/ui/Sidebar";
+import Header from "@/app/components/ui/Header";
+import Generator from "../components/content/Generator";
+
+type Chat = {
+  id: string;
+  messages: { text: string; sender: "user" | "bot" }[];
+  date: string;
+};
 
 const ChatPage = () => {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-400 via-indigo-400 to-purple-500">
-      <div className="max-w-md w-full p-6 bg-white rounded-3xl shadow-xl transform transition-all hover:scale-105">
-        <div className="flex justify-center mb-4">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Imagen de perfil"
-            className="w-32 h-32 rounded-full border-4 border-indigo-500 shadow-lg"
-          />
-        </div>
-        <div className="text-center">
-          <h1 className="text-3xl font-semibold text-gray-800 mb-2">Bienvenido al Chat</h1>
-          <p className="text-lg text-gray-600 mb-6">
-            Únete a nuestra plataforma para comenzar una conversación instantánea y fluida.
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <Link href="/chat/[id]" as="/chat/1">
-            <div className="px-6 py-3 bg-indigo-500 text-white rounded-full shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300">
-              Iniciar Chat
-            </div>
-          </Link>
+  const [activeChat, setActiveChat] = useState<Chat | null>(null);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
 
-          
+  const chatHistory = [
+    { id: 1, title: "Noticias de Tecnología", date: "2024-12-08" },
+    { id: 2, title: "Últimas Noticias de Salud", date: "2024-12-07" },
+  ];
+
+  const groupedChats = chatHistory.reduce(
+    (acc: { [key: string]: typeof chatHistory }, chat) => {
+      const date = chat.date;
+      if (!acc[date]) acc[date] = [];
+      acc[date].push(chat);
+      return acc;
+    },
+    {}
+  );
+
+  return (
+    <div className="flex flex-col gap-4 h-screen bg-background px-6 py-4 ">
+      <Header
+        setSidebarVisible={setSidebarVisible}
+        sidebarVisible={sidebarVisible}
+      />
+      <Sidebar
+        sidebarVisible={sidebarVisible}
+        setSidebarVisible={setSidebarVisible}
+        handleChatSelect={setActiveChat}
+      />
+      <div className="flex h-full">
+        <div className="flex-grow rounded-lg flex flex-col p-4 md:ml-64">
+          <h1>Hi there, !</h1>
+          <p className="text-foreground-secondary text-xl text-center">
+            Let’s transform today’s top stories into engaging content.
+          </p>
+          <div className="mt-6">
+            <Generator />
+          </div>
         </div>
       </div>
     </div>
